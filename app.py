bee61cee2f215315615ec9a1e39a9aa4b72520f0
nielit ropar <nielitropar@users.noreply.huggingface.co>
2023-12-19 07:35:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,40 @@ braintumor_model = load_model('brain_tumor_binary.h5')
 # Configuring Streamlit
 UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-st.set_page_config(page_title="Brain Tumor Prediction App", page_icon=":brain:")
+st.set_page_config(
+    page_title="Brain Tumor Prediction App",
+    page_icon=":brain:",
+    layout="wide"
+)
 
+# Define colors for better styling
+primary_color = "#1f7ed0"
+secondary_color = "#2a2a2a"
+text_color = "#ffffff"
+background_color = "#f4f4f4"
+
+# Set the style for the page
+st.markdown(
+    f"""
+    <style>
+        body {{
+            color: {text_color};
+            background-color: {background_color};
+        }}
+        .reportview-container .main {{
+            color: {text_color};
+            background-color: {background_color};
+        }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Function to allow only specific file types
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# Function to preprocess and crop images
 def preprocess_imgs(set_name, img_size):
     set_new = []
     for img in set_name:
@@ -45,6 +74,7 @@ def crop_imgs(set_name, add_pixels_value=0):
         set_new.append(new_img)
     return np.array(set_new)
 
+# Function to preprocess an image
 def preprocess_image(file_path):
     img = image.load_img(file_path, target_size=(200, 200))
     img_array = image.img_to_array(img)
@@ -52,6 +82,7 @@ def preprocess_image(file_path):
     img_array /= 255.0  # Normalize the image
     return img_array
 
+# Function to make a prediction
 def predict_braintumor(img_path):
     img_gray = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
@@ -70,8 +101,13 @@ def predict_braintumor(img_path):
     else:
         return "Brain Tumor Found!"
 
+# Main function for the app
 def main():
     st.title("Brain Tumor Prediction App")
+
+    # Sidebar for uploading a new image
+    st.sidebar.header("Upload New Image")
+    uploaded_file = st.sidebar.file_uploader("Choose an image...", type=["png", "jpg", "jpeg"])
 
     # Display example images horizontally
     example_images = [
@@ -83,18 +119,38 @@ def main():
         'examples/3 yes.jpg'
     ]
 
+    # Display example images in a row
+    st.write("## Example Images")
     example_col = st.columns(len(example_images))
     for example in example_images:
         example_col[example_images.index(example)].image(example, caption=f"Example: {os.path.basename(example)}", use_column_width=True)
 
-    clicked_example = st.selectbox("Choose an example for prediction:", example_images)
+    st.write("")
 
-    if clicked_example is not None:
-        st.image(clicked_example, caption=f"Selected Example: {os.path.basename(clicked_example)}", use_column_width=True)
+    if uploaded_file is not None:
+        # Display uploaded image
+        st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
         st.write("")
-        st.write("Classifying...")
 
-        # Make prediction for the clicked example
+        # Classify the uploaded image
+        st.write("## Classifying...")
+        result = predict_braintumor(uploaded_file.name)
+
+        # Display prediction
+        st.subheader("Prediction:")
+        st.success(result)
+
+    st.write("")
+
+    # Display selected example image and prediction
+    clicked_example = st.selectbox("Choose an example for prediction:", example_images)
+    if clicked_example is not None:
+        st.write("## Selected Example Image")
+        st.image(clicked_example, caption=f"Example: {os.path.basename(clicked_example)}", use_column_width=True)
+        st.write("")
+
+        # Classify the selected example image
+        st.write("## Classifying...")
         result = predict_braintumor(clicked_example)
 
         # Display prediction
