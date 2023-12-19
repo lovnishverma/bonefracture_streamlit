4c704bdb182ee69f5875f2bced1b122dd2a10b04
nielit ropar <nielitropar@users.noreply.huggingface.co>
2023-12-19 08:56:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -13,81 +13,67 @@ braintumor_model = load_model('brain_tumor_binary.h5')
 # Configuring Streamlit
 UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-st.set_page_config(
-    page_title="Brain Tumor Prediction App",
-    page_icon=":brain:",
-    layout="wide"  # Make the layout wider
-)
+st.set_page_config(page_title="Brain Tumor Prediction App", page_icon=":brain:")
 
-# Add custom CSS for styling
-st.markdown(
-    """
-    <style>
-        .main {
-            max-width: 1200px;
-            margin: auto;
-        }
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-        .title {
-            color: #4285F4;
-            text-align: center;
-            font-size: 36px;
-            margin-bottom: 20px;
-        }
+def preprocess_imgs(set_name, img_size):
+    set_new = []
+    for img in set_name:
+        img = cv2.resize(img, dsize=img_size, interpolation=cv2.INTER_CUBIC)
+        set_new.append(preprocess_input(img))
+    return np.array(set_new)
 
-        .upload-btn-wrapper {
-            position: relative;
-            overflow: hidden;
-            display: inline-block;
-        }
+def crop_imgs(set_name, add_pixels_value=0):
+    set_new = []
+    for img in set_name:
+        gray = cv2.GaussianBlur(img, (5, 5), 0)
+        thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.erode(thresh, None, iterations=2)
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+        c = max(cnts, key=cv2.contourArea)
+        extLeft = tuple(c[c[:, :, 0].argmin()][0])
+        extRight = tuple(c[c[:, :, 0].argmax()][0])
+        extTop = tuple(c[c[:, :, 1].argmin()][0])
+        extBot = tuple(c[c[:, :, 1].argmax()][0])
+        ADD_PIXELS = add_pixels_value
+        new_img = img[extTop[1] - ADD_PIXELS:extBot[1] + ADD_PIXELS,
+                      extLeft[0] - ADD_PIXELS:extRight[0] + ADD_PIXELS].copy()
+        set_new.append(new_img)
+    return np.array(set_new)
 
-        .btn {
-            border: 2px solid gray;
-            color: gray;
-            background-color: white;
-            padding: 8px 20px;
-            border-radius: 8px;
-            font-size: 16px;
-            font-weight: bold;
-        }
+def preprocess_image(file_path):
+    img = image.load_img(file_path, target_size=(200, 200))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.0  # Normalize the image
+    return img_array
 
-        .upload-btn-wrapper input[type=file] {
-            font-size: 100px;
-            position: absolute;
-            left: 0;
-            top: 0;
-            opacity: 0;
-        }
+def predict_braintumor(img_path):
+    img_gray = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
-        .example-images {
-            display: flex;
-            justify-content: space-around;
-            margin-top: 20px;
-        }
+    # Crop and preprocess the grayscale image
+    img_processed = crop_imgs([img_gray])
+    img_processed = preprocess_imgs(img_processed, (224, 224))
 
-        .example-img {
-            width: 150px;
-            height: 150px;
-            object-fit: cover;
-            border-radius: 8px;
-            margin-right: 10px;
-        }
+    # Make prediction
+    pred = braintumor_model.predict(img_processed)
 
-        .prediction {
-            text-align: center;
-            font-size: 24px;
-            margin-top: 20px;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+    # Handle binary decision
+    confidence = pred[0][0]
+
+    if confidence >= 0.5:
+        return "Brain Tumor Not Found!"
+    else:
+        return "Brain Tumor Found!"
 
 def main():
-    st.markdown("<div class='main'>", unsafe_allow_html=True)
-    st.markdown("<div class='title'>Brain Tumor Prediction App</div>", unsafe_allow_html=True)
+    st.title("Brain Tumor Prediction App")
 
-    uploaded_file = st.file_uploader("Choose an image...", type=["png", "jpg", "jpeg"], key="fileuploader")
+    uploaded_file = st.file_uploader("Choose an image...", type=["png", "jpg", "jpeg"])
 
     # Display example images horizontally
     example_images = [
@@ -99,10 +85,9 @@ def main():
         'examples/3 yes.jpg'
     ]
 
-    st.markdown("<div class='example-images'>", unsafe_allow_html=True)
+    example_col = st.columns(len(example_images))
     for example in example_images:
-        st.image(example, caption=f"Example: {os.path.basename(example)}", width=150, use_column_width=False, output_format='auto')
-    st.markdown("</div>", unsafe_allow_html=True)
+        example_col[example_images.index(example)].image(example, caption=f"Example: {os.path.basename(example)}", use_column_width=True)
 
     if uploaded_file is not None:
         st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
@@ -123,11 +108,8 @@ def main():
         result = predict_braintumor(file_path)
 
         # Display prediction
-        st.markdown("<div class='prediction'>", unsafe_allow_html=True)
+        st.subheader("Prediction:")
         st.success(result)
-        st.markdown("</div>", unsafe_allow_html=True)
-
-    st.markdown("</div>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
