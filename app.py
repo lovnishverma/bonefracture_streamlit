dccdb4a30139b0e00cf6e6a55c8fe87c7f91c048
nielit ropar <nielitropar@users.noreply.huggingface.co>
2023-12-19 05:20:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,50 @@
 import streamlit as st
-import os
-from werkzeug.utils import secure_filename
 import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.vgg16 import preprocess_input
 from tensorflow.keras.preprocessing import image
+from werkzeug.utils import secure_filename
+import os
 
-# Load the trained model
-model = load_model("Bone_fracture_classifier_model.h5")
+# Load the brain tumor prediction model
+braintumor_model = load_model('models/brain_tumor_binary.h5')
 
-# Function to check if the file extension is allowed
+# Configuring Streamlit
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'jpg', 'jpeg', 'png'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# Function to preprocess the image
+def preprocess_imgs(set_name, img_size):
+    set_new = []
+    for img in set_name:
+        img = cv2.resize(img, dsize=img_size, interpolation=cv2.INTER_CUBIC)
+        set_new.append(preprocess_input(img))
+    return np.array(set_new)
+
+def crop_imgs(set_name, add_pixels_value=0):
+    set_new = []
+    for img in set_name:
+        gray = cv2.GaussianBlur(img, (5, 5), 0)
+        thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.erode(thresh, None, iterations=2)
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+        c = max(cnts, key=cv2.contourArea)
+        extLeft = tuple(c[c[:, :, 0].argmin()][0])
+        extRight = tuple(c[c[:, :, 0].argmax()][0])
+        extTop = tuple(c[c[:, :, 1].argmin()][0])
+        extBot = tuple(c[c[:, :, 1].argmax()][0])
+        ADD_PIXELS = add_pixels_value
+        new_img = img[extTop[1] - ADD_PIXELS:extBot[1] + ADD_PIXELS,
+                      extLeft[0] - ADD_PIXELS:extRight[0] + ADD_PIXELS].copy()
+        set_new.append(new_img)
+    return np.array(set_new)
+
 def preprocess_image(file_path):
     img = image.load_img(file_path, target_size=(200, 200))
     img_array = image.img_to_array(img)
@@ -21,36 +52,34 @@ def preprocess_image(file_path):
     img_array /= 255.0  # Normalize the image
     return img_array
 
-def main():
-    st.title("Bone Fracture Detection App")
+def predict_braintumor(img_path):
+    img_gray = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    img_processed = crop_imgs([img_gray])
+    img_processed = preprocess_imgs(img_processed, (224, 224))
+    pred = braintumor_model.predict(img_processed)
 
-    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    # Handle binary decision
+    return 1 if pred[0] >= 0.5 else 0
+
+def main():
+    st.title("Brain Tumor Prediction App")
+
+    uploaded_file = st.file_uploader("Choose an image...", type=["png", "jpg", "jpeg"])
 
     if uploaded_file is not None:
-        # Check if the file extension is allowed
-        if allowed_file(uploaded_file.name):
-            # Display the selected image
-            st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+        st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
+        st.write("")
+        st.write("Classifying...")
 
-            # Save the uploaded image temporarily
-            temp_image_path = "temp_image.jpg"
-            with open(temp_image_path, "wb") as temp_image:
-                temp_image.write(uploaded_file.read())
+        # Save the uploaded file
+        filename = secure_filename(uploaded_file.name)
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+        uploaded_file.save(file_path)
 
-            # Preprocess the image
-            img_array = preprocess_image(temp_image_path)
+        # Make prediction
+        result = predict_braintumor(file_path)
 
-            # Make prediction
-            prediction = model.predict(img_array)[0, 0]
-            result = "Broken" if prediction > 0.5 else "Not Broken"
-
-            st.write(f"Prediction: {result}")
-            st.write(f"Confidence: {prediction:.2%}")
-
-            # Remove the temporary image file
-            os.remove(temp_image_path)
-        else:
-            st.warning("Invalid file format. Please upload an image with a valid format (jpg, jpeg, or png).")
+        st.success(f"Prediction: {'Positive' if result == 1 else 'Negative'}")
 
 if __name__ == "__main__":
     main()
